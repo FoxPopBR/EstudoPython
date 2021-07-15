1d5faf9c7482047942302926a8f17044a55b3d86
FoxPop <86852128+FoxPopBR@users.noreply.github.com>
2021-07-15 19:40:07 -0300

    passo 1 no curso python

---
+++ parte1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Começando aprender python ABC
+print ("Hello World")
+print ("Olá Mundo caça")
+print (2*2)
+print (2 ** 3)
+"""
+E aqui temos olá 
+um baita comentário
+varias linhas
+"""
---
+++ parte2.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+
+
+
+
+"""
+lista1 = [1,2,3,4,5,6]
+lista2 = ["Hi","Hello","Bye","TKS","GG"]
+lista3 = [0,"Não","Zica","TMJ",9.13,True,False]
+
+#for i in lista1:
+ #   print(i)
+
+for x in range (0,10,3):
+
+    print (x)
+
+"""
+
+
+
+
+
+
+"""
+
+x = 1
+z = "fim terminou"
+while x < 20:
+    print (x)
+    x = x+1
+  
+print (x,z,"teste aqui")
+
+"""
