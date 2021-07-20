f9929e84807e0df145743a947a940c9b8fcda528
FoxPop <86852128+FoxPopBR@users.noreply.github.com>
2021-07-20 13:00:16 -0300

    fim das alteraçõem em file1, inicio de index

--- Pythondsa/file1.py
+++ Pythondsa/file1.py
@@ -1,14 +1,42 @@
 #-*- coding: -utf8 -*-
 import os
+import _system_path
+import getpass
+
 NomePlayer = ["andreribei00","andreribeir0","AquelaSereia","BangTan 7","Ben Reilly","BlackWidowMF","BR WOLVERINE","Catotinha","dhyessika","Duffly","Fabiiano","FoxPop","Green Vile","hvReus","JEFFER132","Junior crvg","KRATOS 79 BR","KRATOS BR MF","Lady Catota","lalinha44","Lightside 1","Lord Costela","LordRafaeel","MadMax420","MadMoustache","MeniPlays","isaveio","Mx Julielton","Nioljso","O Pae Ta On","princepesam","Rainha Agnes","Rasanzi,Sammy A","Smith Master","sunsupershin","UM DOIS 1 2","Vanda mob","willrick001","Wolverine BR","xX pipoca Xx","xX SAC Xx","xXsaquinXx"] #Lista de membros da guild, separados.. cada nome um item da lista
-dig = 0
+dig = int 
 digop = ""
-op = (int)
 qt = len(NomePlayer)
 
 
+def escolha():
+    if dig == 1:
+        print("Digite o nome:")
+        digop = input()
+        if digop in NomePlayer == True:
+            print("O jogador:",digop,"é um membro da guild!")
+            input()
+            menuo()
+        if digop in NomePlayer == False:
+            print("O jogador:",digop,"não faz parte da guild!")
+            input()
+            menuo()
+    elif dig == 2:
+        print("Quantidade de membros da guild:",qt)     
+        input()
+        menuo()
+    elif dig == 3:
+        os.system('cls')
+        print("Fechando tudo.. Bye")
+        exit
+    else:
+        print("Você entendeu as opções? Faça direito!")
+        input()
+        menuo()
+
+
 def menuo ():
-    os.system('cls') #chama comando do sistema operacional, no caso o comando "cls"
+    #os.system('cls') #chama comando do sistema operacional, no caso o comando "cls"
     dig = 0
     digop = ""
     print("Iniciando treinamento Python\nProjeto controle Moriquendi Federation")
@@ -21,35 +49,10 @@ def menuo ():
     print("==  3- Fechar                                                    ==")
     print("==                                                               ==")
     print("===================================================================\n")
-    dig = input("escolha uma opção: ")
+    print("Escolha uma opção: ")
+    dig = input(int)
+    escolha ()
 
-
-if dig == 1:
-    digop = input("Digite o nome do membro: ")  
-    if digop in NomePlayer == True:
-        print("O jogador:",digop,"é um membro da guild!")
-        input()
-        menuo()
-    elif digop in NomePlayer == False:
-        print("O jogador:",digop,"não faz parte da guild!")
-        input()
-        menuo()
-elif dig == 2:
-    print("Quantidade de membros da guild:",qt)
-    input()   
-    menuo()
-elif dig == 3:
-    os.system('cls')
-    print("Fechando tudo.. Bye")
-    input()
-else:
-    print("Você entendeu as opções? Faça direito!")
-    input()
-    menuo()
-
-
-menuo()
- 
-
+menuo ()
 
 
--- Pythondsa/index.py
+++ Pythondsa/index.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+NomePlayer = ["andreribei00","andreribeir0","AquelaSereia","BangTan 7","Ben Reilly","BlackWidowMF","BR WOLVERINE","Catotinha","dhyessika","Duffly","Fabiiano","FoxPop","Green Vile","hvReus","JEFFER132","Junior crvg","KRATOS 79 BR","KRATOS BR MF","Lady Catota","lalinha44","Lightside 1","Lord Costela","LordRafaeel","MadMax420","MadMoustache","MeniPlays","isaveio","Mx Julielton","Nioljso","O Pae Ta On","princepesam","Rainha Agnes","Rasanzi,Sammy A","Smith Master","sunsupershin","UM DOIS 1 2","Vanda mob","willrick001","Wolverine BR","xX pipoca Xx","xX SAC Xx","xXsaquinXx"] #Lista de membros da guild, separados.. cada nome um item da lista
+dig = int 
+digop = ""
+qt = len(NomePlayer)
+
+print(NomePlayer)
