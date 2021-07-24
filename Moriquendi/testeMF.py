d873f3646bcdebb2d370d0d9ee33f576d77ca7b4
FoxPop <86852128+FoxPopBR@users.noreply.github.com>
2021-07-23 23:05:41 -0300

    Tudo pronto para os testes

--- Moriquendi/testeMF.py
+++ Moriquendi/testeMF.py
@@ -1,13 +1,8 @@
 # -*- coding: -utf8 -*-
-from asyncio.constants import DEBUG_STACK_DEPTH
-from ctypes.wintypes import PINT
-import opcode
-from unittest import TestSuite
-from numpy import dstack
+
 # Importa pacote de funções criados em uma pasta separada
 from classMF.packMF import *
 import os
 import sys
-from mfglobal import *
 
-MenuCad()
+Teste()
