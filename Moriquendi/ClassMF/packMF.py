4f08182e8a9b777f5dcc47878ab2b9960618f5c4
FoxPop <86852128+FoxPopBR@users.noreply.github.com>
2021-07-24 14:13:22 -0300

    tudo pronto para teste final e versionamento

--- Moriquendi/ClassMF/packMF.py
+++ Moriquendi/ClassMF/packMF.py
@@ -1,26 +1,40 @@
 # -*- coding: utf-8 -*-
 """ [Bibliotecas - Pacotes de Funções]
 - Aqui são criadas todas as funções e derivados
-@ Menuo   --> Parte grafica do menu sem interatividade, chama automáticamente a função Escolha
-@ Escolha --> Local para interagir com opções do menu, manipulação de variáveis e parte lógica da interface 
+
+@ Teste --------> Somente para testes rápidos
+@ ListaPlayer --> Gera lista de Jogadores cadastrados com id
+@ MenuLM -------> Desenho do MENU sem interatividade cadastro, remover, buscar, lista, Jogadores
+@ Escolha ------> Local para interagir com opções do menu, manipulação de variáveis e parte lógica da interface 
+@ MenuCad ------> Escolhas do MenuLM / MENU cadastro, remover, buscar, lista, Jogadores
+@ Menuo --------> Parte grafica do menu sem interatividade, chama automáticamente a função Escolha
 
     [extended_summary]
 - Novas funções e derivadas serão criadas aqui!
+By FoxPop / FabioCesar
     """
-from mfglobal import * # NomePlayer,IdJogador,qt,opcao,contador,digop
-import importlib
-import os
-import sys
-from time import time,sleep
+########## IMPORTS INI ##########
+from ast import Try             #1
+from mfglobal import *          #2
+import os                       #3
+import sys                      #4
+from time import time,sleep     #5
+######### IMPORTS END ###########
+
+def Teste():         # ---> Nada aqui sómente para testes rápidos <---
+    print("Local para teste")
 
 
-def ListaPlayer():
-    global NomePlayer,IdJogador,contador,qt
-    os.system("cls")
-    for i in range(qt):
-        print("ID:",IdJogador[i],"",NomePlayer[i])
+def ListaPlayer():   # ---> Lista de jogadores com ID por ordem Alfabetica <---
+    global NomePlayer,IdJogador,contador,qt,qt1
+    os.system ("cls")
+    qt = qt1 = len(NomePlayer)
+    orList = sorted(NomePlayer,key=str.lower) # --> Organiza uma lista em ordem alfabetica <-- obs: chorei sangue aqui @.@
+    for i in range(qt):      
+        print("ID:",IdJogador[i]," ",orList[i])
 
-def MenuLM(): #Função para desenhar o menu simples      ------> Procurar Jogadores <--------
+
+def MenuLM():        # Desenho MENU Simple ---> Procurar Jogadores <---
     os.system("cls") #chama comando do sistema operacional, no caso o comando "cls"
     print("Iniciando treinamento Python\nProjeto controle Moriquendi Federation")
     print("===================================================================")
@@ -38,14 +52,14 @@ def MenuLM(): #Função para desenhar o menu simples      ------> Procurar Jogad
     print("Opções escolhidas:",contador,"\n")
     
 
-# Opções de Menu Inicial
-def Escolha():
+def Escolha():       # Opções de Menu Inicial
     global NomePlayer,IdJogador,contador # Variável precisa ser declarada dentro da função como global para ser modificada
     opcao = input("Escolha uma opção:") #Escolhendo uma opção no menu
     contador = contador + 1
     if opcao == "1":
         MenuCad()     
     elif opcao == "2":      # ------------> Continua a opção 2 do menu principal
+        qt = qt1 = len(NomePlayer)
         print("Quantidade de membros da guild:",qt)     
         input("Enter P/ Continuar..")
         Menuo()
@@ -64,64 +78,88 @@ def Escolha():
         Menuo()
 
 
-def MenuCad ():
-    global NomePlayer,IdJogador,contador
+def MenuCad ():      # ---> MENU CADASTRO JOGADOR <---
+    global NomePlayer,IdJogador,contador,qt # --> Variável global só pode ser chamada uma vez por def
     os.system("cls")
     MenuLM()
     digop = ""
     opcao = input("Escolha uma opção:") #Escolhendo uma opção no menu
-    if opcao == "1":
+    if opcao == "1":    # ---> Buscar Jogador DIGITANDO NOME <---
         digop = input("Buscar Nome:" )
-        if digop in NomePlayer == True:
-            print("O jogador:",digop,"é um membro da guild!")
+        if digop in NomePlayer: # --> 
+            print("O jogador:",digop,"SIM! é um membro da guild! \o/")
             input("Enter P/ Continuar..")   
             MenuCad()  
-        else:  
-            print("O jogador:",digop,"não faz parte da guild!")
+        else:
+            print("O jogador:",digop,"NÃO! faz parte da guild! =s")
             input("Enter P/ Continuar..")
             MenuCad()
-    elif opcao == "2":
-        print("Implementando essa opção, tente mais tarde")
-        MenuCad()
-    elif opcao == "3":
+    elif opcao == "2":     # ---> Buscar Jogador por ID <---  
+        os.system("cls")
+        qt = qt1 = len(NomePlayer)
+        orList = sorted(NomePlayer,key=str.lower)
+        print("Digite o NUMERO ID do Jogador\n")
+        try:    # --> Tente fazer isso até / except
+            t1 = int(input())
+        
+            if t1 <= qt:
+                t2 = t1 - 1
+                print("ID:",t1," ",orList[t2])
+                input("Enter para continuar...")
+                MenuCad()
+            else:
+                print("Esse ID esta vazio, ou não existe!")    
+                input("Enter para continuar..")
+                MenuCad()
+                os.system()     # <--
+        except (TypeError,ValueError):  # ---> Caso try falhe com esses erros faça isso
+            print("Coloque NUMERO do ID valido")    
+            input("Enter para continuar..")
+            MenuCad()   #<----
+    elif opcao == "3":  # ---> Mostra Lista de jogadores com ID <---
         ListaPlayer()
         input("Enter P/ Continuar..")
+        os.system("cls")
         MenuCad()
-    elif opcao == "4":
+    elif opcao == "4":  # ---> ADD Nome de JOGADOR na LISTA <---
         nt = ""
         print("**************************************************************")
         print("*** Insira o nome do jogador a ser add na lista de membros ***")
         print("***                                                        ***")
-        nt = input("Digite o nome:                                            ")
+        nt = input("Digite o nome:")
         NomePlayer.append(nt)
+        print("Nome add:",nt)
+        input("Enter para continuar")
         os.system("cls")
         print("**************************************************************")
         print("*** Insira o nome do jogador a ser add na lista de membros ***")
         print("***                                                        ***")
-        input("Jogador",nt," foi adicionado com sucesso!              ")
+        print("Jogador",nt," foi adicionado com sucesso!")
+        ListaPlayer()
         input("Aperte Enter para retornar...")
         MenuCad()
-    elif opcao == "5":
-        nt = ""
+    elif opcao == "5":  # ---> REMOVER JOGADOR DA LISTA <---
+        nt = str
         print("Digite o nome a ser removido da lista de jogadores\n")
         nt = input("Digite o Nome:")
         NomePlayer.remove(nt)
         print("O nome do Jogador ",nt," Foi removida da lista de membros!")
         input("Aperte Enter para retornar...")
-        MenuLM()
-    elif opcao == "6":
+        MenuCad()
+    elif opcao == "6":  # ---> Retornar ao Menu Principal <---
         print("retornando ao meu principal ~~~")
         sleep(1.3)
         Menuo()
-    else:  # Sempre que não digitar uma opção valida
+    elif opcao == "7":
+        exit()
+    else:  # Sempre que não digitar uma opção invalida
         print("Você entendeu as opções? Faça direito!")
         print("Você digitou: ",opcao)
         input("Enter P/ Continuar..")
-        MenuLM()    
-    
-# Menu Inicial Aparência 
+        MenuCad()  
 
-def Menuo(): #Função para desenhar o menu simples       -----> MENU PRINCIPAL  <------
+
+def Menuo():         # ---> MENU PRINCIPAL <--- Desenho
     os.system("cls") #chama comando do sistema operacional, no caso o comando "cls"
     print("Iniciando treinamento Python\nProjeto controle Moriquendi Federation")
     print("===================================================================")
@@ -135,6 +173,4 @@ def Menuo(): #Função para desenhar o menu simples       -----> MENU PRINCIPAL 
     print("===================================================================\n")
     print("Opções escolhidas:",contador,"\n")
     Escolha()
-
-
 
